65c5cdc6f324dde02d859c761ed9276a36b520f2
Prasanga Fernando <prasangafdz@outlook.com>
2021-11-06 17:13:09 +0530

    Wrote the first data to the excel sheet

--- src/test/java/com/jxl/write/Runner.java
+++ src/test/java/com/jxl/write/Runner.java
@@ -10,7 +10,11 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
 import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
 
 public class Runner {
 
@@ -20,18 +24,22 @@ public class Runner {
 	
 	static WritableWorkbook writeWB;
 	@Test
-	public void openBrowser() throws BiffException, IOException {
+	public void openBrowser() throws BiffException, IOException, RowsExceededException, WriteException {
 		
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://mvnrepository.com/");
-		
-		String fileLocation = "src/test/resources/ExcelToWrite.xls";
+//		WebDriverManager.chromedriver().setup();
+//		WebDriver driver = new ChromeDriver();
+//		driver.get("https://mvnrepository.com/");
+//		
+		String fileLocation = "src/test/resources/Book.xls";
 		File file = new File(fileLocation);
 		Workbook wb = Workbook.getWorkbook(file);
 		writeWB = Workbook.createWorkbook(new File(fileLocation), wb);
+		WritableSheet sheet = writeWB.getSheet(0);
+		Label label;
 		
-		
-		
+		label = new Label(0,0,"Cell 0,0"); //Here we put the cell location and the data to write
+		sheet.addCell(label);
+		writeWB.write();
+		writeWB.close();
 	}
 }
